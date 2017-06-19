a5e2cb51e88cad37e720d1d2df7a47be5e3d2279
imad Collin <imad.collin@gmail.com>
2017-06-19 16:47:17 +0200

    Add category mock data

---
+++ BookLibraryEF/Models/CategoryMockData.cs
@@ -0,0 +1,20 @@
+﻿using AspCoreLibraryEF.wwwroot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspCoreLibraryEF.Models
+{
+    public class CategoryMockData : ICategoryRepo
+    {
+        public IEnumerable<Category> Categories =>
+            
+        new List<Category>  {
+                     new Category{CategoryId=1, CategoryName="Fiction", Discription="This Category contain some data"},
+                     new Category{CategoryId=2, CategoryName="Drama", Discription="This Category contain drama "},
+                     new Category{CategoryId=3, CategoryName="Poet", Discription="This Category contain Potry"}
+
+        };
+    }
+}
