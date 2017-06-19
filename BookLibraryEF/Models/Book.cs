f8a796562c3fd8f2244444fe6131f2ca5e98d0b6
imad Collin <imad.collin@gmail.com>
2017-06-19 16:38:46 +0200

    Add book model

---
+++ BookLibraryEF/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspCoreLibraryEF.wwwroot.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Discription { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime PublishDate { get; set; }
+       
+        public bool InStock { get; set; }
+
+        public int CategoryId  { get; set; }
+    }
+}
