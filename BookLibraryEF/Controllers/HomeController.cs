8af639ad3c53410b1388d3f034553ca10226eb50
imad Collin <imad.collin@gmail.com>
2017-06-19 17:26:37 +0200

    Generate a list of book in Home Controller

--- BookLibraryEF/Controllers/HomeController.cs
+++ BookLibraryEF/Controllers/HomeController.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using AspCoreLibraryEF.wwwroot.Models;
 
 namespace BookLibraryEF.Controllers
 {
     public class HomeController : Controller
     {
+        private IBookRepository _bookRepo;
+        private ICategoryRepo _categoryRepo;
+
+        public HomeController(IBookRepository _book, ICategoryRepo _category) {
+            _bookRepo= _book ;
+             _categoryRepo= _category;
+        }
         public IActionResult Index()
         {
+            Console.WriteLine(_bookRepo.Books);
             return View();
         }
 
--- BookLibraryEF/Models/BookMockData.cs
+++ BookLibraryEF/Models/BookMockData.cs
@@ -14,7 +14,7 @@ namespace AspCoreLibraryEF.Models
         public IEnumerable<Book> Books =>
             new List<Book> {
             new Book { ID = 1, Title = "Sun Rise", Discription = "When the sun rise", Price = 240, ImageUrl = "", PublishDate = new DateTime(1981, 03, 01), InStock = true, CategoryId = 1, Category= _ICategory.Categories.ToList()[0] },
-            new Book { ID = 2, Title = "Sun Set", Discription = "When the sun set", Price = 500, ImageUrl = "", PublishDate = new DateTime(1984, 03, 01), InStock = true, CategoryId = 3, Category= _ICategory.Categories.ToList()[3] }
+            new Book { ID = 2, Title = "Sun Set", Discription = "When the sun set", Price = 500, ImageUrl = "", PublishDate = new DateTime(1984, 03, 01), InStock = true, CategoryId = 3, Category= _ICategory.Categories.ToList()[1] }
         };
 
         public IEnumerable<Book> MustCommon => throw new NotImplementedException();
