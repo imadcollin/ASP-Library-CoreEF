8ba6b0b3a918ccc376a26fc80da1e3400bb1d14d
imad Collin <imad.collin@gmail.com>
2017-06-19 16:43:51 +0200

    Add an Interface for Book

---
+++ BookLibraryEF/Models/IBookRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspCoreLibraryEF.wwwroot.Models
+{
+    public interface IBookRepository
+    {
+        IEnumerable<Book> Books { get; }
+        IEnumerable<Book> MustCommon { get; }
+        Book GetBookById(int id);
+    }
+}
